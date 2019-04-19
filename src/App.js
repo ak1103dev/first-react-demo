83cccff5a222298de1151eb844fa46ce5d3752b7
ak1103dev <ak.apichan1103@gmail.com>
2019-04-19 20:57:45 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="app-left">
+          <img
+            className="image"
+            alt="profile"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTURpbzTRCeuOGMyYZbtuNnrjDac4nI5MJM0Q45BEEqEu6y9uc6Gg"
+          />
+          <h1>Sara Watson</h1>
+          <h2>About</h2>
+          <p>Lorem ipsumkfjdlkajklfjlkjfkldjklaj kljlfkeajl kjkljfkla jkjk fjkej kljl</p>
+          <p>Lorem ipsumkfjdlkajklfjlkjfkldjklaj kljlfkeajl kjkljfkla jkjk fjkej kljl</p>
+          <p>Lorem ipsumkfjdlkajklfjlkjfkldjklaj kljlfkeajl kjkljfkla jkjk fjkej kljl</p>
+          <p>Lorem ipsumkfjdlkajklfjlkjfkldjklaj kljlfkeajl kjkljfkla jkjk fjkej kljl</p>
+        </div>
+        <div className="app-right">
+          <h2 className="title">Experiences</h2>
+          <h2 className="title">Educations</h2>
+          <h2 className="title">Hobbies</h2>
+        </div>
       </div>
     );
   }
